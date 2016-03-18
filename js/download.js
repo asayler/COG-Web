bb6b3e85212d9f248878fab6c4e6be13718f08e6
Taylor Andrews <taylor.andrews-1@colorado.edu>
2016-03-18 14:56:20 -0600

    Download code finished with webpage redirect

--- js/download.js
+++ js/download.js
@@ -1,13 +1,33 @@
 function initiate_download() {
 	file_uuid = getParameterByName('uuid');
 
-	file_get_contents(log_download, submit_error_callback, file_uuid);
+    file_get(get_contents, submit_error_callback, file_uuid);
+
+    function get_contents(data, status) {
+        var filename = data[file_uuid].name;
+        $("span#file_name").text(filename);
+        file_get_contents(log_download, submit_error_callback, file_uuid);
+
+        function log_download(data, status) {
+            console.log("downloading:" + JSON.stringify(data));
+            download(filename, data);
+        }
+    }
 }
 
-function log_download(data, status) {
+// Taken from http://stackoverflow.com/questions/3665115/create-a-file-in-memory-for-user-to-download-not-through-server
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
 
-	console.log(JSON.stringify(data));
-	console.log("Download done");
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+    console.log("Download done");
 }
 
 // Taken from http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
